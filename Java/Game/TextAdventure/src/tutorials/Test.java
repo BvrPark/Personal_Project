cd6c61c0b603efb09682ee594a096771187b7673
crupy <wlxh95@naver.com>
2022-03-30 21:52:15 +0900

    경험치를 적용하기전에 테스트함

---
+++ Java/Game/TextAdventure/src/tutorials/Test.java
@@ -0,0 +1,11 @@
+package tutorials;
+
+import java.util.*;
+
+public class Test {
+    public static void main(String[] args) {
+        Random r = new Random();
+        double enemyExp = r.nextInt(150)/10.0;
+        System.out.println(enemyExp);
+    }
+}
