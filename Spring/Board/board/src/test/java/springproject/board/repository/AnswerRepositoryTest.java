e65065c1abe28c9c0eac99b1cedc20f2d8407fc1
-crupy- <wlghsms95@gmail.com>
2023-02-23 23:58:44 +0900

    MemberRepository와 AnswerRepository 테스트
    MemberRepository에서 멤버 단건 조회, 멤버 여러 명 조회, 삭제 등을 테스트하고
    AnswerRepository에서 질문에 대한 답변 단건 조회, 답변 전체 조회를 테스트했다.

---
+++ Spring/Board/board/src/test/java/springproject/board/repository/AnswerRepositoryTest.java
@@ -0,0 +1,74 @@
+package springproject.board.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import springproject.board.domain.Answer;
+import springproject.board.domain.Member;
+import springproject.board.domain.Question;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class AnswerRepositoryTest {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @Test
+    public void 답변조회() throws Exception{
+        //given
+        Question question = new Question();
+        question.setSubject("제목입니다.");
+        question.setContent("본문입니다.");
+        questionRepository.save(question);
+
+        Answer answer = new Answer();
+        answer.setContent("답변입니다.");
+        answer.setCreateDate(LocalDateTime.now());
+        answer.setQuestion(questionRepository.findById(question.getId()).get());
+        answerRepository.save(answer);
+
+        //when
+        Answer findByIdAnswer = answerRepository.findById(answer.getId()).get();
+
+        //then
+        assertEquals(findByIdAnswer.getQuestion(),question);
+
+    }
+
+    @Test
+    public void 답변전체조회() throws Exception{
+        //given
+        Question question = new Question();
+        question.setSubject("제목입니다.");
+        question.setContent("본문입니다.");
+        questionRepository.save(question);
+        Question findQuestion = questionRepository.findById(question.getId()).get();
+
+        Answer answer = new Answer();
+        answer.setContent("답변입니다.");
+        answer.setQuestion(findQuestion);
+        answer.setCreateDate(LocalDateTime.now());
+        answerRepository.save(answer);
+
+        //when
+        List<Answer> answerList = answerRepository.answerList(findQuestion);
+
+        //then
+        assertThat(answerList.get(0).getContent()).isEqualTo("답변입니다.");
+
+    }
+
+}
---
+++ Spring/Board/board/src/test/java/springproject/board/repository/MemberRepositoryTest.java
@@ -0,0 +1,58 @@
+package springproject.board.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import springproject.board.domain.Member;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    public void 기본CRUD() throws Exception{
+        //given
+        Member member1 = new Member();
+        Member member2 = new Member();
+        member1.setName("member1");
+        member2.setName("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //when
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+
+        //then
+
+        //단건 조회
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        //리스트 조회
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        //카운트 검증
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        //삭제
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        long deleteCount = memberRepository.count();
+        assertThat(deleteCount).isEqualTo(0);
+    }
+
+}
