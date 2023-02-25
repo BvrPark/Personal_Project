e2851e50350a721e0c5fe10bea37703566f7d68f
-crupy- <wlghsms95@gmail.com>
2023-02-25 23:32:37 +0900

    QuestionRepository에 페이징 추가
    QuestionRepository에 Question 전체 조회를 하는 메서드 추가

--- Spring/Board/board/src/main/java/springproject/board/repository/QuestionRepository.java
+++ Spring/Board/board/src/main/java/springproject/board/repository/QuestionRepository.java
@@ -1,6 +1,11 @@
 package springproject.board.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import springproject.board.domain.Answer;
 import springproject.board.domain.Question;
 
 import java.time.LocalDateTime;
@@ -10,5 +15,7 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
     Question findBySubject(String subject);
     Question findByContent(String content);
     List<Question> findBySubjectLike(String subject);
+
+    Page<Question> findAll(Pageable pageable);
 
 }
