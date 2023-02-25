1c19205c215c660fc5220a3c5f865f8f4b7e7cae
-crupy- <wlghsms95@gmail.com>
2023-02-25 23:31:39 +0900

    @Query 사용
    @Query를 사용하여 Answer전체를 조회하는 로직을 작성

--- Spring/Board/board/src/main/java/springproject/board/repository/AnswerRepository.java
+++ Spring/Board/board/src/main/java/springproject/board/repository/AnswerRepository.java
@@ -1,7 +1,16 @@
 package springproject.board.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import springproject.board.domain.Answer;
+import springproject.board.domain.Question;
+
+import java.util.List;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
+
+    @Query("select a from Answer a where a.question = :question")
+    List<Answer> answerList(@Param("question") Question question);
+
 }
