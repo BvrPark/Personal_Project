ade3d1b2ef547a16087fbbf46823dc8bff2c3c4f
crupy <>
2023-04-14 20:45:13 +0900

    no message

---
+++ Java/Report_project/jdbc-MySQL/src/BoardApplication.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class BoardApplication {
+    public static void main(String[] args) {fffff
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+    }
+}
